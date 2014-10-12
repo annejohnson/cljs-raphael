87cf090d15b82d8f22a70625bfd0b1f898e8e818
Anne Johnson <annekjohnson23@yahoo.com>
2014-10-12 16:58:49 -0400

    Correct URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cljs-raphael "0.1.0-SNAPSHOT"
   :description "Clojurescript/Raphael funsies"
-  :url "http://annejohnson.github.io/clojurescript-raphael"
+  :url "http://annejohnson.github.io/cljs-raphael"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
